1c3bda2dca53b72a373ad4df624774fd906382c8
Andrey Nazarov <andrexnazarov@gmail.com>
2024-12-04 00:22:45 +0300

    Изменен дизайн, добавлен плавный индикатор

--- lib/StartView/StartView.dart
+++ lib/StartView/StartView.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:ratrace/StartView/Indicator.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'CustomPageViewScrollPhisics.dart';
 
 class StartView extends StatefulWidget {
@@ -15,44 +15,66 @@ class StartView extends StatefulWidget {
 
 class _StartViewState extends State<StartView> {
   final List<String> _images = [
-    'assets/image/image1.png',
-    'assets/image/image2.png',
-    'assets/image/img.png'
+    'assets/image/jobs/lawyer.png',
+    'assets/image/jobs/pilot.png',
+    'assets/image/jobs/teacher.png'
   ];
 
-  int _currentIndex = 0;
+  final List<String> jobs = [
+    "Юрист",
+    "Пилот Авиалиний",
+    "Учитель",
+  ];
 
-  PageController controller = PageController(initialPage: 0);
+  PageController controller = PageController(viewportFraction: 0.9, initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [PageView(
-          controller: controller,
-          physics: const CustomPageViewScrollPhysics(),
-          children: _images
-              .map((image) => Image.asset(image))
-              .toList(),
-          onPageChanged: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                // color: Colors.red,
-                child: Indicator(controller, 3),
-            )],
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 500,
+            child: PageView.builder(
+              padEnds: true,
+              controller: controller,
+              physics: const CustomPageViewScrollPhysics(),
+              itemCount: _images.length,
+              itemBuilder: (_, index) => Container (
+                margin: const EdgeInsets.all(9),
+                decoration: BoxDecoration (
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.amber,
+                  image: DecorationImage (
+                    image: AssetImage(_images[index]),
+                    fit: BoxFit.cover
+                  )
+                ),
+              ),
+            ),
           ),
-        )
+          SizedBox(
+            height: 50,
+            child: Center(
+              child: Center(
+                child: SmoothPageIndicator (
+                  controller: controller,
+                  count: _images.length,
+                  effect: const WormEffect(),
+                  onDotClicked: (index) => controller.animateToPage (
+                      index,
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.decelerate
+                  ),
+                ),
+              ),
+            )
+          ),
         ],
       )
     );
   }
 }
+
+
+
